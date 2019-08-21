6342804f01053085a1197a391146cfec86d2604e
Robin Millette <robin@millette.info>
2019-08-21 16:17:06 -0400

    Search parent dir for .git dir.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-'use strict'
+"use strict"
 
 // core
-const fs = require('fs')
+const fs = require("fs")
+const path = require("path")
 
 // npm
-const git = require('isomorphic-git')
+const git = require("isomorphic-git")
 
-git.plugins.set('fs', fs)
+// console.log('GIT KEYS', Object.keys(git.E))
+// const { E: { ResolveRefError } } = git
+// console.log('ResolveRefError', ResolveRefError)
 
-const doit0 = (commits) => commits.reduce((a, ts) => {
-  const el = a.length && ts - a[a.length - 1].ts
-  a.push({ ts, el })
-  return a
-}, [])
+git.plugins.set("fs", fs)
+
+const doit0 = (commits) =>
+  commits.reduce((a, ts) => {
+    const el = a.length && ts - a[a.length - 1].ts
+    a.push({ ts, el })
+    return a
+  }, [])
 
 const doit1 = (max, els) => {
   const p2 = [[]]
@@ -34,24 +40,46 @@ const doit2 = (max, p2) => {
     if (n > 1) p3.push({ el: p[n - 1].ts - p[0].ts, n })
     else ++extras
   })
-  if (extras) p3.push({ el: extras * max / 4, n: extras })
+  if (extras) p3.push({ el: (extras * max) / 4, n: extras })
   return p3
 }
 
 const doit = (max, commits) => doit2(max, doit1(max, doit0(commits)))
 
-const vava = (dir, max) => git.log({ dir })
-  .then((commits) => doit(max, commits.reverse().map(({ author: { timestamp }}) => timestamp)))
-
-const stats = (x) => {
-  const s = x.reduce((a, b) => {
-    return {
-      el: a.el + b.el,
-      n: a.n + b.n
+const vava = async (dir, max) => {
+  try {
+    const commits = await git.log({ dir })
+    return doit(
+      max,
+      commits.reverse().map(({ author: { timestamp } }) => timestamp)
+    )
+  } catch (e) {
+    if (e.name === "ResolveRefError") {
+      const d2 = path.resolve(dir, "..")
+      if (dir === d2) throw e
+      return vava(d2, max)
     }
-  }, {el:0, n:0})
-  const mc = Math.round(s.el / (s.n * 60))
-  console.log(`${x.length} spurts; ${Math.round(s.el / 360)/10}h total; ${s.n} commits; ${Math.round(s.n / x.length)} commits/spurt; ${mc} min./commit.`)
+    throw e
+  }
 }
 
-module.exports = (opts = {}) => vava(opts.dir || '.', (opts.max || 120) * 60).then(stats)
+const stats = (x) => {
+  const s = x.reduce(
+    (a, b) => {
+      return {
+        el: a.el + b.el,
+        n: a.n + b.n,
+      }
+    },
+    { el: 0, n: 0 }
+  )
+  const mc = Math.round(s.el / (s.n * 60))
+  console.log(
+    `${x.length} spurts; ${Math.round(s.el / 360) / 10}h total; ${
+      s.n
+    } commits; ${Math.round(s.n / x.length)} commits/spurt; ${mc} min./commit.`
+  )
+}
+
+module.exports = (opts = {}) =>
+  vava(opts.dir || ".", (opts.max || 120) * 60).then(stats)
